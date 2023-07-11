0e9c0890068982e2ec7d32c6ce1ebec470b13516
Karlo Secen <karlosecen10@gmail.com>
2023-07-11 13:19:45 +0200

    Initial commit

---
+++ src/main/java/com/example/shorty/account/Account.java
@@ -0,0 +1,44 @@
+package com.example.shorty.account;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Account {
+    @Id
+    @SequenceGenerator(
+            name = "account_sequence",
+            sequenceName = "account_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "account_sequence"
+    )
+    private String accountId;
+    private String password;
+
+    public Account() {
+    }
+
+    public Account(String accountId, String password) {
+        this.accountId = accountId;
+        this.password = password;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/example/shorty/account/AccountService.java
@@ -0,0 +1,21 @@
+package com.example.shorty.account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public List<Account> getAccounts() {
+        //return new Account("johnny","pass123");
+        return accountRepository.findAll();
+    }
+}
